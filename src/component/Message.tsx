53df1eadbfbc40e14bd96a540e43d0998f9ed2bb
Registrat00r <ipasa0085@gmail.com>
2025-06-04 16:51:49 +0000

    1 урок

---
+++ src/component/Message.tsx
@@ -0,0 +1,10 @@
+import './Message.module.css'
+
+
+export default function MessageFunc() {
+    return (
+        <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+        </p>
+    )
+};

d37c07c6d17a43140bbbd4502f12dd26952113f7
Registrat00r <ipasa0085@gmail.com>
2025-06-10 20:06:29 +0000

    Лекция 2

--- src/components/Main/creator/Creator.tsx
+++ src/components/Main/creator/Creator.tsx
@@ -12,13 +12,13 @@ export const Creator = (): React.ReactElement => {
                 <button className="creator__button button-sign" type="button">Попробуйте это прямо сейчас</button>
             </div>
             <div className="creator__card__container">
-                 {/* <div className="creator__card  creator__card-1">
+                 <div className="creator__card  creator__card-1">
                     <div className="creator__card__content">
                         <p className="creator__card__description">На Splice я всегда могу найти то, что ищу, будь то именно
                             тот звук,
                             который мне нужен, или просто немного вдохновения.</p>
-                        <img className="creator__card__image" src="img/photo/people_andrew.png" alt="Эндрю Хуанг">
-                        <p className="creator__card__name">Эндрю Хуанг<br><span className="creator__card__role"></span>Артист
+                        <img className="creator__card__image" src="img/photo/people_andrew.png" alt="Эндрю Хуанг"/>
+                        <p className="creator__card__name">Эндрю Хуанг<br/><span className="creator__card__role"></span>Артист
                         </p>
 
                     </div>
@@ -30,8 +30,8 @@ export const Creator = (): React.ReactElement => {
                         <p className="creator__card__description">Наконец-то способ купить плагины, который работает. Платя
                             понемногу за
                             раз, продюсеры могут получить законный доступ к лучшим VST.</p>
-                        <img className="creator__card__image" src="img/photo/people_kshmr.png" alt="KSHMR">
-                        <p className="creator__card__name">KSHMR<br><span className="creator__card__role"></span>Aртист</p>
+                        <img className="creator__card__image" src="img/photo/people_kshmr.png" alt="KSHMR"/>
+                        <p className="creator__card__name">KSHMR<br/><span className="creator__card__role"></span>Aртист</p>
                     </div>
 
                 </div>
@@ -39,8 +39,8 @@ export const Creator = (): React.ReactElement => {
                     <div className="creator__card__content">
                         <p className="creator__card__description">Было интересно погрузиться в сообщество создателей Splices и изучить инструменты,
                              которые поддерживают мой творческий процесс.</p>
-                        <img className="creator__card__image" src="img/photo/people_kesha.png" alt="Кеша">
-                        <p className="creator__card__name">Кеша<br><span className="creator__card__role"></span>Aртист</p>
+                        <img className="creator__card__image" src="img/photo/people_kesha.png" alt="Кеша"/>
+                        <p className="creator__card__name">Кеша<br/><span className="creator__card__role"></span>Aртист</p>
                     </div>
 
                 </div>
@@ -48,12 +48,12 @@ export const Creator = (): React.ReactElement => {
                     <div className="creator__card__content">
                         <p className="creator__card__description">Я всегда могу найти на Splice то, что ищу,
                              будь то нужный мне звук или просто источник вдохновения.</p>
-                        <img className="creator__card__image" src="img/photo/people_andrew.png" alt="Эндрю Хуанг">
-                        <p className="creator__card__name">Эндрю Хуанг<br><span className="creator__card__role"></span>Aртист
+                        <img className="creator__card__image" src="img/photo/people_andrew.png" alt="Эндрю Хуанг"/>
+                        <p className="creator__card__name">Эндрю Хуанг<br/><span className="creator__card__role"></span>Aртист
                         </p>
                     </div>
 
-                </div> */}
+                </div>
 
             </div>
 
--- src/components/Main/sounds/Sounds.tsx
+++ src/components/Main/sounds/Sounds.tsx
@@ -5,33 +5,39 @@ export const Sounds = (): React.ReactElement => {
         <section className="section sounds">
             <h4 className="sounds__subtitle">Звуки</h4>
             <h2 className="sounds__title">Найди свой <span className="sounds__title-span">звук</span></h2>
-            <p className="sounds__description">Просматривайте и загружайте миллионы бесплатных сэмплов<br/>
-                от ведущих продюсеров, артистов и звукорежиссеров. Доступно<br/>
+            <p className="sounds__description">Просматривайте и загружайте миллионы бесплатных сэмплов<br />
+                от ведущих продюсеров, артистов и звукорежиссеров. Доступно<br />
                 на настольных компьютерах, в Интернете и мобильных устройствах.</p>
             <button className="sounds__button button-sign" type="button">Попробуй звук</button>
 
-            <div className="swiper">
-                {/* Additional required wrapper */}
-                <div className="swiper-wrapper">
-                    {/* Slides */}
-                    <div className="swiper-slide">
-                        <img className="sounds__image" src="img/sound_packs.png"
-                            alt="Программное обеспечение для создания музыки"/>
-                    </div>
-                    <div className="swiper-slide">
-                        <img className="sounds__image" src="img/sound_packs.png"
-                            alt="Программное обеспечение для создания музыки"/>
-                    </div>
-                    <div className="swiper-slide">
-                        <img className="sounds__image" src="img/sound_packs.png"
-                            alt="Программное обеспечение для создания музыки"/>
-                    </div>
 
+             <div className="sounds__img">
+                    <img className="sounds__image" src="img/sound_packs.png" alt="Програмное обеспечение для создания музыки"/>    
+                    
                 </div>
-                <div className="swiper-button-prev"></div>
-                <div className="swiper-button-next"></div>
+
+            {/* <div className="swiper">
+                {/* Additional required wrapper
+            <div className="swiper-wrapper">
+                {/* Slides 
+                <div className="swiper-slide">
+                    <img className="sounds__image" src="img/sound_packs.png"
+                        alt="Программное обеспечение для создания музыки" />
+                </div>
+                <div className="swiper-slide">
+                    <img className="sounds__image" src="img/sound_packs.png"
+                        alt="Программное обеспечение для создания музыки" />
+                </div>
+                <div className="swiper-slide">
+                    <img className="sounds__image" src="img/sound_packs.png"
+                        alt="Программное обеспечение для создания музыки" />
+                </div>
 
             </div>
-        </section>
+            <div className="swiper-button-prev"></div>
+            <div className="swiper-button-next"></div>
+
+        </div> */}
+        </section >
     )
 }

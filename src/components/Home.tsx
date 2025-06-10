3a15e5474f49fabd9b1456f45839be58b1c1e654
Registrat00r <ipasa0085@gmail.com>
2025-06-10 22:49:28 +0000

    Лекция 3

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,6 +8,7 @@ import { Creator } from "./Main/creator/Creator"
 import { Customization } from "./Main/customization/Customization"
 import { Jobs } from "./Main/jobs/Jobs"
 
+
 export const Home = (): React.ReactElement => {
     return (
         <>

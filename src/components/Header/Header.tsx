d28a8edf91a209983490e69937e744f638a4ae03
Registrat00r <ipasa0085@gmail.com>
2025-06-24 14:34:55 +0000

    Форма

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router'
+import { Form } from "../form/Form"
 
 export const Header = (): React.ReactElement => {
+        const [formOpen, setFormOpen] = useState(false)
      return (
         <header className="header container">
         <div className="header__logo logo">
@@ -22,11 +24,12 @@ export const Header = (): React.ReactElement => {
 
             <div className="header__buttons">
                 <button className="header__login button-log" type="button">Войти</button>
-                <button className="header__sign button-sign" type="button">Зарегистрироваться</button>
+                <button className="header__sign button-sign" type="button" onClick={() => setFormOpen(true)}>Зарегистрироваться</button>
             </div>
 
         </nav>
 
+        <Form isOpen={formOpen} onClose={() => setFormOpen(false)} />
     </header>
      )
 }
---
+++ src/components/form/Form.tsx
@@ -0,0 +1,52 @@
+import type React from 'react';
+import type { PropsWithChildren } from 'react';
+import ReactDOM from 'react-dom'
+
+type ModalProps = {
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+export const Form = ({isOpen, onClose}: PropsWithChildren<ModalProps>) => {
+
+    const Submit = (e: React.FormEvent) => {
+         e.preventDefault();
+         onClose();
+    }
+
+    if(!isOpen) return null
+    return ReactDOM.createPortal(
+        <div className="form" id="formModal" onClick={onClose}>
+        <div className="contact-form-container" onClick={(e) => e.stopPropagation()}>
+            <div className="form-menu">
+                <h2 className="form__name">Зарегистрироваться</h2>
+                <button className="exit-button" onClick={onClose}>X</button>
+            </div>
+            <form id="contactForm" onSubmit={Submit}>
+                <div className="form-group">
+                    <label htmlFor="surname">Фамилия</label>
+                    <input type="text" id="surname" name="surname" required/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="name">Имя</label>
+                    <input type="text" id="name" name="name" required/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Почта</label>
+                    <input type="email" id="email" name="email" required/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Пароль</label>
+                    <input type="password" id="password" name="password" required/>
+                </div>
+                <div className="form-buttons">
+                    <button type="submit" className="submit-button">Зарегистрироваться</button>
+                    <button type="button" className="cansel-button" onClick={onClose}>Выход</button>
+                </div>
+
+            </form>
+        </div>
+    </div>,
+    document.body
+    )
+}
